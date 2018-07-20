99896f88673e1c8811c0cb8c59cc273ad5474acb
curtischatch <curtis.chat.hk@gmail.com>
2018-07-20 12:49:46 -0400

    the cat works

---
+++ exercise2.rb
@@ -0,0 +1,21 @@
+class Cat
+  def initialize(name, preferred_food, meal_time)
+    @name - name
+    @prefferred_food = preferred_good
+    @meal_time = meal_time
+  end
+
+  #instance methods
+  def eats_at
+    if @meal_time > 12
+      return "#{@meal_time-12}pm"
+    elsif @meal_time == 12
+      "#{@meal_time}pm"
+    else
+      "#{@meal_time} am"
+    end
+  end
+
+  def meow
+    return "Meow, I am #{@name} and I eat #{@prefferred_food} at #{eat_at}"
+  end 
